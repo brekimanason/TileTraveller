b40f6eab4418d952aa18a333441c80e49c1d2db6
Bjarturf20 <bjarturfinnboga@gmail.com>
2020-09-17 16:14:47 +0000

    Lausn

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,24 +1,95 @@
-#coordinates
-x = 1
-y = 1
 
-room1_1 = "(N)orth"
-room1_2 = "(N)orth or (S)outh or (E)ast"
-room1_3 = "(S)outh or (E)ast"
-room2_1 = "(N)orth"
-room2_2 = "(S)outh or (W)est"
-room2_3 = "(W)est or (E)ast"
-room3_1 = "Victory!"
-room3_2 = "(N)orth or (S)outh"
-room3_3 = "(S)outh or (W)est"
-path = room1_1
+# https://github.com/brekimanason/TileTraveller
 
-path_available = 1
+# Geri Fall fyrir hvert herbergi og svo hoppar notandi milli falla og þar með milli herbergja
+def room1_1():
+    print("You can travel (N)orth.")
+    direction = input("Direction: ")
+    if direction == "n" or direction == "N":
+        room1_2()
+    else:
+        print("Not a valid direction!")
+        room1_1()
 
-while 1:
+def room1_2():
+    print("You can travel: (N)orth or (E)ast or (S)outh.")
+    direction = input("Direction: ")
+    if direction == "n" or direction == "N":
+        room1_3()
+    elif direction == "e" or direction == "E":
+        room2_2()
+    elif direction == "s" or direction == "S":
+        room1_1()
+    else:
+        print("Not a valid direction!")
+        room1_2()
 
-    if path_available = 1:
-        print("You can travel:", path)
-    
-    
-    direction = input("Direction: ").lower
+def room1_3():
+    print("You can travel: (E)ast or (S)outh.")
+    direction = input("Direction: ")
+    if direction == "e" or direction == "E":
+        room2_3()
+    elif direction == "s" or direction == "S":
+        room1_2()
+    else:
+        print("Not a valid direction!")
+        room1_3()
+
+def room2_1():
+    print("You can travel: (N)orth.")
+    direction = input("Direction: ")
+    if direction == "n" or direction == "N":
+        room2_2()
+    else:
+        print("Not a valid direction!")
+        room2_1()
+
+def room2_2():
+    print("You can travel: (S)outh or (W)est.")
+    direction = input("Direction: ")
+    if direction == "s" or direction == "S":
+        room2_1()
+    elif direction == "w" or direction == "W":
+        room1_2()
+    else:
+        print("Not a valid direction!")
+        room2_2()
+
+def room2_3():
+    print("You can travel: (E)ast or (W)est.")
+    direction = input("Direction: ")
+    if direction == "e" or direction == "E":
+        room3_3()
+    elif direction == "w" or direction == "W":
+        room1_3()
+    else:
+        print("Not a valid direction!")
+        room2_3()
+
+def room3_1():
+    print("Victory!")
+
+def room3_2():
+    print("You can travel: (N)orth or (S)outh.")
+    direction = input("Direction: ")
+    if direction == "n" or direction == "N":
+        room3_3()
+    elif direction == "s" or direction == "S":
+        room3_1()
+    else:
+        print("Not a valid direction!")
+        room3_2()
+
+def room3_3():
+    print("You can travel: (S)outh or (W)est.")
+    direction = input("Direction: ")
+    if direction == "s" or direction == "S":
+        room3_2()
+    elif direction == "w" or direction == "W":
+        room2_3()
+    else:
+        print("Not a valid direction!")
+        room3_3()
+
+
+room1_1()
