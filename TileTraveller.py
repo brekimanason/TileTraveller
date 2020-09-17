5234a82c14d52ec6b8511eb2d11f0d0926675259
Bjarturf20 <bjarturfinnboga@gmail.com>
2020-09-17 10:35:51 +0000

    Comment

---
+++ TileTraveller.py
@@ -0,0 +1,24 @@
+#coordinates
+x = 1
+y = 1
+
+room1_1 = "(N)orth"
+room1_2 = "(N)orth or (S)outh or (E)ast"
+room1_3 = "(S)outh or (E)ast"
+room2_1 = "(N)orth"
+room2_2 = "(S)outh or (W)est"
+room2_3 = "(W)est or (E)ast"
+room3_1 = "Victory!"
+room3_2 = "(N)orth or (S)outh"
+room3_3 = "(S)outh or (W)est"
+path = room1_1
+
+path_available = 1
+
+while 1:
+
+    if path_available = 1:
+        print("You can travel:", path)
+    
+    
+    direction = input("Direction: ").lower
